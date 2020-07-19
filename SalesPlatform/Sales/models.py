5e62941f18c26900130a92ab4e24f1092f56d313
Stanislav.Ivanov <stanislav_ivanov9213@abv.bg>
2020-07-19 22:28:45 +0300

    Models initialize

--- SalesPlatform/Sales/models.py
+++ SalesPlatform/Sales/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+USER_POSITION = [
+    ('Top','Founder/Co-Founder'),
+    ('Manager','Manager'),
+    ('Dev','Developer'),
+    ('SalesRep','SalesRepresentative')
+]
+class Users(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    position = models.CharField(max_length=15,choices=USER_POSITION)
+    users = models.Manager()
 
+class Contacts(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    contact_details = models.CharField(max_length=300)
+    contact_id = models.ForeignKey(Users,on_delete=models.PROTECT)
+    contacts = models.Manager() 
 # Create your models here.
