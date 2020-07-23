036dc3b63c98e26418778595097b0087dd1fa765
Stanislav.Ivanov <stanislav_ivanov9213@abv.bg>
2020-07-23 12:23:07 +0300

    Added Views

--- SalesPlatform/Sales/views.py
+++ SalesPlatform/Sales/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
-
+from Sales.models import Users,Contacts
+from Sales.serializers import UserSerializer,ContactSerializer
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
 # Create your views here.
+def user_list(request):
+    """
+    List all code users, or create a new user.
+    """
+    if request.method == 'GET':
+        users = Users.users.all()
+        serializer = UserSerializer(users, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request.body)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+def contact_list(request):
+    """
+    List all contacts, or create a new contact.
+    """
+    if request.method == 'GET':
+        contacts = Contacts.contacts.all()
+        serializer = ContactSerializer(contacts, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request.body)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)        
